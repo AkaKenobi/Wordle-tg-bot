be50770d9194e30206b0997377e3e1c304dcb5a2
dosankule-lab <dosankule@gmail.com>
2026-05-18 01:02:06 +0500

    hiii

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def print_hi(name):
     print(f'Hi, {name}')
 if __name__ == '__main__':
     print_hi('PyCharm')
-#баарвараыдвррадврл
+#баарвараыдвррадврл вловждщпзждбьвплзхыжьбвпщзэжиьюжпзщэ
 print("test")
-print("Yera Roma Islam")
+print("Yera Romaп Islam")
 print("Yera Roma 45764965мммсчм894Islam")
