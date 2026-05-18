d7f60ee526b03bd07c86c1697e239b3943a0661e
dosankule-lab <dosankule@gmail.com>
2026-05-19 00:34:30 +0500

    Yerassyl Dossankul

--- main.py
+++
@@ -1,8 +0,0 @@
-def print_hi(name):
-    print(f'Hi, {name}')
-if __name__ == '__main__':
-    print_hi('PyCharm')
-#баарвараыдвррадврл вловждщпзждбьвплзхыжьбвпщзэжиьюжпзщэ
-print("test")
-print("Yera Romaп Islam")
-print("Yera Roma 45764965мммсчм894Islam")
